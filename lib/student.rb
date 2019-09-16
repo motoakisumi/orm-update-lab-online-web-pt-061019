e979c01c8d65fc08d70246f67cc1165baf03eb05
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-16 00:23:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,8 +7,9 @@ class Student
   def initialize(id = nil, name, grade)
     @name = name
     @grade = grade
+    @id = id
   end
-  
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
