c2c550123d061b3ac14bca41dd35154456c157ae
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-16 00:34:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE  id = ?"
-    DB[:conn].executute(sql, self.name, self.grade, self.id)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 
